583991adcc0ea9e8ab0df69e0eb0e8ea6049faab
Dwayne R. Crooks <me@dwaynecrooks.com>
2014-05-11 07:14:24 -0400

    Add another example, examples/case_changer.rb

---
+++ examples/case_changer.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+#
+# See http://en.wikipedia.org/wiki/State_pattern#Java
+
+require 'state_pattern'
+
+class CaseChanger < StatePattern::StateMachine
+  def start_state_class
+    Lowercase
+  end
+end
+
+class CaseChangerState < StatePattern::BaseState
+  def_actions :write
+end
+
+class Lowercase < CaseChangerState
+  def write(name)
+    puts name.downcase
+    state_machine.transition_to_state(MultipleUppercase)
+  end
+end
+
+class MultipleUppercase < CaseChangerState
+  def write(name)
+    puts name.upcase
+    state_machine.transition_to_state(Lowercase) if incr_count > 1
+  end
+
+  private
+    def incr_count
+      @count = 0 unless @count
+      @count += 1
+    end
+end
+
+def main
+  changer = CaseChanger.new
+
+  ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].each do |day|
+    changer.write(day)
+  end
+end
+
+main
