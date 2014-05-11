b9416ef1774412a5d244073465b504f051938ba6
Dwayne R. Crooks <me@dwaynecrooks.com>
2014-05-11 15:22:57 -0400

    Separate out the public API in the StatePattern::StateMachine class

--- lib/state_pattern/state_machine.rb
+++ lib/state_pattern/state_machine.rb
@@ -13,38 +13,20 @@ module StatePattern
       setup_delegation
     end
 
-    def initialize_context
-      @ctx = initial_context
+    def initial_context
     end
 
-    def transition_to_start_state
-      transition_to_state(start_state)
-    end
-
-    def transition_to_state(state_class)
-      @state = state_class.new(self)
-    end
-
-    def setup_delegation
-      setup_context_delegation
-      setup_action_delegation
-    end
-
-    def setup_context_delegation
-      if @ctx
-        methods = @ctx.members.map { |reader| [reader, "#{reader}=".to_sym] }.flatten
-        self.class.def_delegators :@ctx, *methods
-      end
-    end
-
-    def setup_action_delegation
-      methods = @state.actions
-      self.class.def_delegators :@state, *methods
+    def start_state
+      raise NotImplementedError
     end
 
     def transition_to_state_and_send_event(state_class, name, message = {})
       transition_to_state(state_class)
       send_event(name, message)
+    end
+
+    def transition_to_state(state_class)
+      @state = state_class.new(self)
     end
 
     def send_event(name, message = {})
@@ -56,11 +38,31 @@ module StatePattern
       self
     end
 
-    def initial_context
-    end
+    private
 
-    def start_state
-      raise NotImplementedError
-    end
+      def initialize_context
+        @ctx = initial_context
+      end
+
+      def transition_to_start_state
+        transition_to_state(start_state)
+      end
+
+      def setup_delegation
+        setup_context_delegation
+        setup_action_delegation
+      end
+
+      def setup_context_delegation
+        if @ctx
+          methods = @ctx.members.map { |reader| [reader, "#{reader}=".to_sym] }.flatten
+          self.class.def_delegators :@ctx, *methods
+        end
+      end
+
+      def setup_action_delegation
+        methods = @state.actions
+        self.class.def_delegators :@state, *methods
+      end
   end
 end
